78a2c84b94bf0b11cac93192b1572c7edcab7aab
Yabin Cui <splintcoder@gmail.cm>
2017-04-01 12:33:13 -0700

    init.

---
+++ main.py
@@ -0,0 +1,64 @@
+# Copyright 2016 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import time
+import webapp2
+import logging
+import os
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.write('<p>Hello, World!</p>')
+        self.response.write("I am yabin cui!")
+
+class TimeoutPage(webapp2.RequestHandler):
+    def get(self):
+        from google.appengine.runtime import DeadlineExceededError
+        try:
+            time.sleep(70)
+            self.response.write('Completed.')
+        except DeadlineExceededError:
+            self.response.clear()
+            self.response.set_status(500)
+            self.response.out.write('The request did not complete in time.')
+
+class LogPage(webapp2.RequestHandler):
+    def get(self):
+        logging.debug('This is a debug message')
+        logging.info('This is an info message')
+        logging.warning('This is a warning message')
+        logging.error('This is an error message')
+        logging.critical('This is a critical message')
+        
+        try:
+            raise ValueError('This is a sample value error.')
+        except ValueError:
+            logging.exception('An example exception log.')
+        
+        self.response.out.write('Logging example.')
+
+class EnvPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        for key, value in os.environ.iteritems():
+            self.response.out.write('{} = {}\n'.format(key, value))
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+    ('/timeout', TimeoutPage),
+    ('/log', LogPage),
+    ('/env', EnvPage),
+], debug=True)
